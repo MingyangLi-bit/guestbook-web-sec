d4e4161863c55e8aec909d0f24f335c645bf9cbd
Sruti Bhagavatula <srutib@cmu.edu>
2024-04-17 11:39:42 -0500

    Changed let to const in a few places + fixed db.destroy()

--- src/pages/api/get_all_messages.js
+++ src/pages/api/get_all_messages.js
@@ -1,6 +1,6 @@
 import { getDatabase, closeDBInstance } from "@/lib/db";
 
-const getAllMessages = async (db, queryParams) => {
+const getAllMessages = async (db) => {
     return new Promise((resolve, reject) => {
         const query = 'SELECT name, address, message FROM messages WHERE display = 1;';
         console.log(query);
@@ -17,13 +17,13 @@ const getAllMessages = async (db, queryParams) => {
 export default async function handler(req, res) {
     const db = getDatabase();
     try {
-        const messages = await getAllMessages(db, req.query);
+        const messages = await getAllMessages(db);
         closeDBInstance(db);
         res.status(200).json(messages);
     } catch (e) {
         console.error(e);
         closeDBInstance(db);
-        res.status(400).json("Error getting messages");
+        res.status(400).json(e.message);
     }
 
 }
